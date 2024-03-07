1f88da8d352e04ab53e193b03abc054fcfa75271
falcon71181 <abhaythakur71181@gmail.com>
2024-03-07 18:50:30 +0530

    Resume Added + Removed Default svg

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -4,11 +4,12 @@ import ThemeSwitcher from "@/app/ThemeSwitcher";
 type navItem = {
   name: string;
   href: string;
+  target: "_self" | "_blank";
 };
 
 const navItems: navItem[] = [
-  { name: "blog", href: "/blog" },
-  { name: "resume", href: "/resume" },
+  { name: "blog", href: "/blog", target: "_self" },
+  { name: "resume", href: "/resume.pdf", target: "_blank" },
 ];
 
 const NavBar = () => {
@@ -21,10 +22,11 @@ const NavBar = () => {
         falcon71181
       </Link>
       <section className="flex items-center gap-4 md:gap-7 transition-all duration-500">
-        {navItems.map(({ name, href }) => (
+        {navItems.map(({ name, href, target }) => (
           <Link
             key={name + href}
             href={href}
+            target={target}
             className="text-black dark:text-white text-lg md:text-xl font-medium hover:text-hover_link_light dark:hover:text-hover_heading transition-all duration-500"
           >
             {name}
