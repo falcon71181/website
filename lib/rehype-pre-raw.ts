c82dfb78fc78cb58db42bcb189aae8b41dbd459e
falcon71181 <abhaythakur71181@gmail.com>
2024-03-16 18:49:19 +0530

    Blog utilities Added

---
+++ lib/rehype-pre-raw.ts
@@ -0,0 +1,21 @@
+import { visit } from 'unist-util-visit';
+
+export const preProcess = () => (tree: any) => {
+  visit(tree, (node) => {
+    if (node?.type === 'element' && node?.tagName === 'pre') {
+      const [codeEl] = node.children;
+
+      if (codeEl.tagName !== 'code') return;
+
+      node.raw = codeEl.children?.[0].value;
+    }
+  });
+};
+
+export const postProcess = () => (tree: any) => {
+  visit(tree, 'element', (node) => {
+    if (node?.type === 'element' && node?.tagName === 'pre') {
+      node.properties['raw'] = node.raw;
+    }
+  });
+};
