f7412a9cc4a9e407c704801ab3d944c501d67e28
falcon71181 <abhaythakur71181@gmail.com>
2024-04-08 07:45:09 +0530

    Minor Styling

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 };
 export default function Home() {
   return (
-    <main className="px-0 sm:px-2 md:px-5 lg:px-9 w-full h-full flex flex-col items-center text-black dark:text-heading border-2 border-red-300">
+    <main className="px-0 sm:px-2 md:px-5 lg:px-9 w-full min-h-[88vh] flex flex-col items-center text-black dark:text-heading border-2 border-red-300">
       <About />
       <GridCards />
       <Projects />
@@ -19,9 +19,9 @@ export default function Home() {
 
 const Projects = () => {
   return (
-    <section className="mt-5 flex flex-col gap-3">
-      <h1 className="font-semibold text-lg">Featured Projects</h1>
-      <div className="sm:px-2 md:px-4 lg:px-5 grid grid-cols-3 gap-4">
+    <section className="mt-5 flex flex-col gap-3 transition-all duration-200">
+      <h1 className="font-semibold text-base md:text-xl">Featured Projects :-</h1>
+      <div className="sm:px-2 md:px-4 lg:px-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {repos.map((repo) => (
           <RepoCard key={repo.name} repo={repo} />
         ))}
