cf6d27de5bad18490f5d4b7ed90385f0e66e86eb
falcon71181 <abhaythakur71181@gmail.com>
2024-05-04 00:06:14 +0530

    style: minior styling dix

--- components/foot/Footer.tsx
+++ components/foot/Footer.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
   return (
     <div className='pb-8'>
       <div className='border border-[#242F2B] mb-8'></div>
-      <div className='flex justify-between items-center text-xs text-[#e1e7ef]/80'>
+      <div className='flex justify-between items-center text-xs dark:text-[#e1e7ef]/80'>
         <h1>© 2024 falcon71181. All rights reserved.</h1>
-        <h1><a className='underline underline-offset-4 decoration-[#e1e7ef]/40 hover:decoration-[#e1e7ef]/80' href='https://github.com/falcon71181/website' target='_blank'>Source</a> for nerds</h1>
+        <h1><a className='underline underline-offset-4 dark:decoration-[#e1e7ef]/40 dark:hover:decoration-[#e1e7ef]/80' href='https://github.com/falcon71181/website' target='_blank'>Source</a> for nerds</h1>
       </div>
     </div>
   )
